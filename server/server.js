a88f30d7b875f58d0a4ea6ac1ca8dd2458d85198
kumar.vikram <kumar.vikram@samsung.com>
2019-02-18 14:51:59 +0530

    Added connect and disconnect events on server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
-
 app.use(express.static(publicPath));
-app.listen(port,()=>{
-    console.log('Server is om port : ', port);
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log('new user connected');
+    socket.on('disconnect',()=>{
+        console.log('user was disconnected.');
+;    });
+});
+
+server.listen(port,()=>{
+    console.log('Server is on port : ', port);
 });
 
